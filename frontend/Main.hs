08cfcd3e759f813ee3c749500d65918f191fcdc7
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-04-02 13:53:21 +0200

    Basic calculator

--- frontend/Main.hs
+++ frontend/Main.hs
@@ -1,4 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Reflex
+import Reflex.Dom
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Text (pack, unpack, Text)
+import Text.Read (readMaybe)
+import Control.Applicative ((<*>), (<$>))
+
+main = mainWidget $ el "div" $ do
+  nx <- numberInput
+  d <- dropdown Times (constDyn ops) def
+  ny <- numberInput
+  let values = zipDynWith (,) nx ny
+      result = zipDynWith (\o (x,y) -> runOp o <$> x <*> y) (_dropdown_value d) values
+      resultText = fmap (pack . show) result
+  text " = "
+  dynText resultText
+
+numberInput :: MonadWidget t m => m (Dynamic t (Maybe Double))
+numberInput = do
+  n <- textInput $ def & textInputConfig_inputType .~ "number"
+                       & textInputConfig_initialValue .~ "0"
+  return . fmap (readMaybe . unpack) $ _textInput_value n
+
+data Op = Plus | Minus | Times | Divide deriving (Eq, Ord)
+
+ops :: Map Op Text
+ops = Map.fromList [(Plus, "+"), (Minus, "-"), (Times, "*"), (Divide, "/")]
+
+runOp :: Fractional a => Op -> a -> a -> a
+runOp s = case s of
+            Plus -> (+)
+            Minus -> (-)
+            Times -> (*)
+            Divide -> (/)
